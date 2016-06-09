42977fa697d1ac1fb7e968938cf97b06b9c0e1e8
FrogInABox <epickaimail@gmail.com>
2016-06-09 06:03:56 +0100

    Updated symlink script to ignore .git folder in addition to .gitignore.

--- symlinks.sh
+++ symlinks.sh
@@ -4,7 +4,7 @@ echo "Creating symlinks for all dotfiles..."
 # symlink all root dotfiles to home directory
 for file in .*
 do
-	if [ "$file" != ".gitignore" ]
+	if [ "$file" != ".gitignore" ] && [ "$file" != ".git" ]
 	then
 		ln -s $(pwd)/$file ~
 	fi
