7dc0a00ace6be9b75b1f0975ac40cd35c8402456
Kai Leddy <epickaimail@gmail.com>
2018-09-28 10:46:59 +0100

    bspwm - fix desktop count when reloading config

--- bspwm/.config/bspwm/monitors.sh
+++ bspwm/.config/bspwm/monitors.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ "$(bspc query -D | wc -l)" -ne "10" ]; then
+    bspc monitor eDP1 -d 1 2 3 4 5 6 7 8 9 10
+fi
+
 if [ "$(bspc query -M | wc -l)" -eq "2" ]; then
     for d in {1,2,3,4,5}; do bspc desktop $d -m eDP1 ; done
     for d in {6,7,8,9,10}; do bspc desktop $d -m HDMI1 ; done
