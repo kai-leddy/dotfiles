8514cc29ce7a4fb84d23d00a5a1b46fc49a3f682
Kai Leddy <kai@utto.com>
2018-10-22 13:12:17 +0100

    Updated fish functions

--- fish/.config/fish/functions/gi.fish
+++ fish/.config/fish/functions/gi.fish
@@ -1,3 +1,3 @@
-function gi
+function gi --description "produce a gitignore for a filetype"
 	curl -L -s https://www.gitignore.io/api/$argv
 end
--- fish/.config/fish/functions/ugz.fish
+++ fish/.config/fish/functions/ugz.fish
@@ -1,3 +1,3 @@
 function ugz --description 'unzip .tar.gz with pigz'
-	unpigz -v --stdout $argv[1] | tar -zxvf -
+	unpigz -v --stdout $argv[1] | tar -xvf -
 end
