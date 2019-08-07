fa29c5d5cc1ec2a1d060b3b6c51e78032914f929
Kai Leddy <epickaimail@gmail.com>
2019-08-07 13:59:49 +0100

    Add config for dual external monitor setup

--- bspwm/.config/bspwm/monitors.sh
+++ bspwm/.config/bspwm/monitors.sh
@@ -3,21 +3,39 @@
 # kill polybar instances so they can be restarted on correct monitors
 killall polybar
 
-# make sure there is exactly 10 desktops
-if [ "$(bspc query -D | wc -l)" -ne "10" ]; then
+# make sure there is always at least 10 desktops
+if [ "$(bspc query -D | wc -l)" -lt "10" ]; then
     bspc monitor eDP1 -d 1 2 3 4 5 6 7 8 9 10
 fi
 
+monitor_count="$(xrandr | grep ' connected' | wc -l)"
+
 # map desktops to the correct monitors and restart polybar instances
-if [ "$(xrandr | grep ' connected' | wc -l)" -eq "2" ]; then
-    for d in {1,2,3,4,5}; do bspc desktop $d -m eDP1 ; done
-    for d in {6,7,8,9,10}; do bspc desktop $d -m HDMI1 ; done
+if [ "$monitor_count" -eq "3" ]
+then
+    for d in {1..4}; do bspc desktop $d -m eDP1 ; done
+    for d in {5..7}; do bspc desktop $d -m HDMI1 ; done
+    for d in {8..10}; do bspc desktop $d -m DP2 ; done
+    bspc wm --reorder-monitors eDP1 HDMI1 DP2
+    bspc desktop Desktop --remove
+    bspc desktop Desktop --remove
+    polybar external &
+    polybar external_extra &
+    polybar main &
+elif [ "$monitor_count" -eq "2" ]
+then
+    for d in {1..5}; do bspc desktop $d -m eDP1 ; done
+    for d in {6..9}; do bspc desktop $d -m HDMI1 ; done
+    # fix for 10 being residual and already being in the wrong place
+    bspc desktop 10 -m eDP1
+    bspc desktop 10 -m HDMI1
+    # end fix
     bspc wm --reorder-monitors eDP1 HDMI1
     bspc desktop Desktop --remove
     polybar external &
     polybar main &
 else
-    for d in {1,2,3,4,5,6,7,8,9,10}; do bspc desktop $d -m eDP1 ; done
+    for d in {1..10}; do bspc desktop $d -m eDP1 ; done
     polybar main &
 fi
 
