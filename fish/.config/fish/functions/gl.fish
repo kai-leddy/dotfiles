1b5831afad7cc2fef58ad3251caa9ba6e07549f8
Kai Leddy <epickaimail@gmail.com>
2019-10-15 16:15:35 +0100

    Added git alias functions for fish

---
+++ fish/.config/fish/functions/gl.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function gl --description 'alias gl git lg'
+	git lg $argv;
+end
---
+++ fish/.config/fish/functions/gs.fish
@@ -0,0 +1,4 @@
+# Defined in /tmp/fish.Oe8QNo/gs.fish @ line 2
+function gs --description 'alias gs git status'
+	git status $argv;
+end
