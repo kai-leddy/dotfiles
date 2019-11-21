aaf907656ee6f922f886e4bf3bf4971b65e02080
Kai Leddy <epickaimail@gmail.com>
2019-11-21 11:34:01 +0000

    Switch to a btops workflow for bspwm

--- bspwm/.config/bspwm/monitors.sh
+++ bspwm/.config/bspwm/monitors.sh
@@ -5,39 +5,19 @@ killall polybar
 
 # put monitors in the order I want for iterating in
 # including those which should never exist, just in case
-bspc wm --reorder-monitors eDP-1 DVI-I-1-1 DVI-I-2-2
+bspc wm --reorder-monitors HDMI-1 eDP-1 DP-1
 
-#monitors="$(xrandr | grep ' connected' | awk '{print $1}')"
-mapfile -t monitors < <(bspc query -M --names)
+mapfile -t monitors < <(xrandr | grep ' connected' | awk '{print $1}')
 monitor_count=${#monitors[@]}
 
-# make sure there is always at least 10 desktops
-if [ "$(bspc query -D | wc -l)" -lt "10" ]; then
-    bspc monitor eDP-1 -d 1 2 3 4 5 6 7 8 9 10
-fi
-
-# map desktops to the correct monitors and restart polybar instances
-if [ "$monitor_count" -eq "3" ]
-then
-    for d in {1..4}; do bspc desktop $d -m ${monitors[0]} ; done
-    for d in {5..7}; do bspc desktop $d -m ${monitors[1]} ; done
-    for d in {8..10}; do bspc desktop $d -m ${monitors[2]} ; done
-    bspc desktop Desktop --remove
-    bspc desktop Desktop --remove
-    MONITOR=${monitors[2]} polybar external_extra &
-    MONITOR=${monitors[1]} polybar external &
-    MONITOR=${monitors[0]} polybar main &
-elif [ "$monitor_count" -eq "2" ]
-then
-    for d in {1..5}; do bspc desktop $d -m ${monitors[0]} ; done
-    for d in {6..10}; do bspc desktop $d -m ${monitors[1]} ; done
-    bspc desktop Desktop --remove
-    MONITOR=${monitors[1]} polybar external &
-    MONITOR=${monitors[0]} polybar main &
-else
-    for d in {1..10}; do bspc desktop $d -m ${monitors[0]} ; done
-    MONITOR=${monitors[0]} polybar main &
-fi
+# create polybar instances for each monitor
+for mon in "${monitors[@]}"; do
+    if [ "$mon" = "eDP-1" ]; then
+        MONITOR=$mon polybar main &
+    else
+        MONITOR=$mon polybar external &
+    fi
+done
 
 # re-initialise the wallpaper to force tiling instead of stretching
 feh --bg-fill ~/.config/wallpaper
