0b1c4e94c610c91d2a4c843b7fd063f8540f1d82
Kai Leddy <epickaimail@gmail.com>
2020-10-21 15:30:51 +0100

    Slightly more usable emacs

--- nixos/modules/emacs.nix
+++ nixos/modules/emacs.nix
@@ -1,4 +1,22 @@
-{ pkgs, options, lib, config, ... }: {
-  #TODO: upgrade to emacs 27
-  environment.systemPackages = with pkgs; [ emacs nixfmt ];
+{ pkgs, options, lib, config, ... }:
+
+let unstable = import <unstable> { config = config.nixpkgs.config; };
+in {
+  # TODO: use Emacs 28 emacsGcc from the overlay
+  environment.systemPackages = with pkgs; [
+    # Required for doom emacs to work properly
+    unstable.emacs # 27.1
+    git
+    ripgrep
+
+    # optional stuff
+    fd # faster projectile indexing
+    zstd # for undo-tree compression
+
+    # Module dependencies
+    nixfmt # :lang nix
+    nodePackages.javascript-typescript-langserver # :lang javascript
+  ];
+
+  fonts.fonts = [ pkgs.emacs-all-the-icons-fonts ];
 }
