d66f2ef33173ae8764e89e36e71324dd025f3b67
Kai Leddy <epickaimail@gmail.com>
2020-10-21 13:04:18 +0100

    Fix btops and nerd-font

--- nixos/modules/bspwm.nix
+++ nixos/modules/bspwm.nix
@@ -2,22 +2,8 @@
 
 let
   unstable = import <unstable> { config = config.nixpkgs.config; };
-  # TODO: put this somewhere else
-  btops = with pkgs;
-    buildGoPackage {
-      pname = "btops";
-      version = "1.0.0";
-      goPackagePath = "github.com/cmschuetz/btops";
-
-      src = fetchFromGitHub {
-        owner = "cmschuetz";
-        repo = "btops";
-        rev = "b7f90a54af5cfc86460fc1acaa4b3fa156ad634f";
-        sha256 = "1xzhbvc0lxdjh4bsc3g93fv68zbhn7rdn9m391p5fnxnxb3ikbgp";
-      };
-
-      goDeps = ./btops-deps.nix;
-    };
+  btops = pkgs.callPackage ../pkgs/btops { };
+  fantasque-nerdfont = pkgs.callPackage ../pkgs/fantasque-nerdfont.nix { };
 in {
   services.xserver = {
     windowManager.bspwm = { enable = true; };
@@ -29,7 +15,9 @@ in {
 
   environment.systemPackages = with pkgs; [ btops polybar rofi ];
 
-  fonts.fonts =
-    [ (unstable.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; }) ];
+  fonts.fonts = [
+    # (unstable.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    fantasque-nerdfont # custom derivation due to issues with above v2.1.0
+  ];
 
 }
---
+++ nixos/pkgs/fantasque-nerdfont.nix
@@ -0,0 +1,30 @@
+# NOTE: this is a custom derivation for the FantasqueSansMono Nerd Font
+# I did this because `unstable.nerdfonts` refers to v2.1.0 which
+# has outstanding issues with the font-width of various glyphs.
+# https://github.com/ryanoasis/nerd-fonts/issues/442
+
+{ stdenv, fetchzip }:
+
+let
+  repo = "https://github.com/ryanoasis/nerd-fonts";
+  version = "2.0.0";
+in stdenv.mkDerivation {
+  name = "fantasque-nerdfont";
+  version = version;
+
+  src = fetchzip {
+    url = "${repo}/releases/download/v${version}/FantasqueSansMono.zip";
+    sha256 = "0z54ib9s9sj0l85gc53nbcrcjncxhj14jssmryzq6bc79pyd5nkr";
+    stripRoot = false;
+  };
+
+  buildCommand = ''
+    install --target $out/share/fonts/truetype -D $src/*.ttf
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Nerdfont version of Fantasque Sans Mono";
+    homepage = repo;
+    license = licenses.mit;
+  };
+}
