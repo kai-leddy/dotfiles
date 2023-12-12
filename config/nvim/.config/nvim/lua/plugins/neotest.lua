dbac6c7e3306c0a396800525326551998798b677
Kai Leddy <epickaimail@gmail.com>
2023-12-12 13:40:53 +0000

    Update neovim plugins & fix neotest

--- config/nvim/.config/nvim/lua/plugins/neotest.lua
+++ config/nvim/.config/nvim/lua/plugins/neotest.lua
@@ -1,9 +1,34 @@
 return {
   {
     "nvim-neotest/neotest",
-    dependencies = { "nvim-neotest/neotest-jest" },
+    event = "LspAttach",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-neotest/neotest-jest",
+    },
     opts = {
-      adapters = { "neotest-jest" },
+      adapters = {
+        ["neotest-jest"] = {
+          -- jestCommand = "npx jest --watch ", -- disabled as it currently provides no pass/fail feedback
+          jest_test_discovery = true,
+        },
+      },
+      discovery = {
+        -- this only works with neotest-jest and jest_test_discovery = true
+        -- if you ever test other languages/adapters, turn this on again and jest_test_discovery = false
+        enabled = false,
+      },
+    },
+  },
+  {
+    "folke/neodev.nvim",
+    opts = {
+      library = {
+        plugins = { "neotest" },
+        types = true,
+      },
     },
   },
 }
