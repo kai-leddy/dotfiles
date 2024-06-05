86ee0a1833a4eaabf91e63502f7149edb601bc23
Kai Leddy <epickaimail@gmail.com>
2024-06-05 10:19:43 +0100

    Update hammerspoon config to allow moving windows to other screens

--- config/hammerspoon/.hammerspoon/init.lua
+++ config/hammerspoon/.hammerspoon/init.lua
@@ -3,20 +3,37 @@ hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
 -- Keybindings
-mash = {"cmd", "alt", "ctrl"}
+mash = { "cmd", "alt", "ctrl" }
+shiftMash = { "cmd", "alt", "ctrl", "shift" }
 
 hs.hotkey.bind(mash, "h", function()
-                 hs.window.focusedWindow():focusWindowWest(nil,nil,true)
+	hs.window.focusedWindow():focusWindowWest(nil, nil, true)
 end)
 
 hs.hotkey.bind(mash, "j", function()
-                 hs.window.focusedWindow():focusWindowSouth(nil,nil,true)
+	hs.window.focusedWindow():focusWindowSouth(nil, nil, true)
 end)
 
 hs.hotkey.bind(mash, "k", function()
-                 hs.window.focusedWindow():focusWindowNorth(nil,nil,true)
+	hs.window.focusedWindow():focusWindowNorth(nil, nil, true)
 end)
 
 hs.hotkey.bind(mash, "l", function()
-                 hs.window.focusedWindow():focusWindowEast(nil,nil,true)
+	hs.window.focusedWindow():focusWindowEast(nil, nil, true)
+end)
+
+hs.hotkey.bind(shiftMash, "h", function()
+	hs.window.focusedWindow():moveOneScreenWest()
+end)
+
+hs.hotkey.bind(shiftMash, "j", function()
+	hs.window.focusedWindow():moveOneScreenSouth()
+end)
+
+hs.hotkey.bind(shiftMash, "k", function()
+	hs.window.focusedWindow():moveOneScreenNorth()
+end)
+
+hs.hotkey.bind(shiftMash, "l", function()
+	hs.window.focusedWindow():moveOneScreenEast()
 end)
