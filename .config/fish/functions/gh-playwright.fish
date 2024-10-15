dbbdc920678ce0abffeb54b4fedc5716aaecc25a
Kai Leddy <epickaimail@gmail.com>
2024-10-15 17:09:10 +0100

    Add function to show playwright reports from GH actions

--- .config/fish/functions/gh-playwright.fish
+++ .config/fish/functions/gh-playwright.fish
@@ -1,9 +1,9 @@
 function gh-playwright
-    set selected_workflow (gh run list --status=failure --limit 100 --json name,displayTitle,headBranch,updatedAt,databaseId | jq -r '.[] | "\(.name)\t\(.headBranch)\t\(.displayTitle)\t\(.updatedAt)\t\(.databaseId)\n"' | column -t -s \t -x | fzf | awk 'NF{print $NF}')
+    set selected_workflow (gh run list --status=failure --limit 100 --json name,displayTitle,headBranch,updatedAt,databaseId | jq -r '.[] | "\(.name)\t\(.headBranch)\t\(.displayTitle)\t\(.updatedAt)\t\(.databaseId)\n"' | column -t -s \t -x | fzf | awk 'NF {print $NF}')
 
     if test -n "$selected_workflow"
         set temp_dir (mktemp -d)
-        gh run download $run_id --name playwright-report --dir $temp_dir
+        gh run download $selected_workflow --name playwright-report --dir $temp_dir
         pnpm dlx playwright show-report $temp_dir
         rm -rf $temp_dir
     else
