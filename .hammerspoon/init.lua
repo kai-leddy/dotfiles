ff1d87b722678e802db65ea6a02b017ceab4553c
Kai Leddy <epickaimail@gmail.com>
2024-10-21 15:36:53 +0100

    Test aerospace with proper config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -10,39 +10,6 @@ spoon.SpoonInstall:andUse("ReloadConfiguration", { start = true })
 -- Local libs
 local toggleVPN = require("vpn")
 
--- Keybindings
-hs.hotkey.bind(mash, "h", function()
-	hs.window.focusedWindow():focusWindowWest(nil, nil, true)
-end)
-
-hs.hotkey.bind(mash, "j", function()
-	hs.window.focusedWindow():focusWindowSouth(nil, nil, true)
-end)
-
-hs.hotkey.bind(mash, "k", function()
-	hs.window.focusedWindow():focusWindowNorth(nil, nil, true)
-end)
-
-hs.hotkey.bind(mash, "l", function()
-	hs.window.focusedWindow():focusWindowEast(nil, nil, true)
-end)
-
-hs.hotkey.bind(shiftMash, "h", function()
-	hs.window.focusedWindow():moveOneScreenWest()
-end)
-
-hs.hotkey.bind(shiftMash, "j", function()
-	hs.window.focusedWindow():moveOneScreenSouth()
-end)
-
-hs.hotkey.bind(shiftMash, "k", function()
-	hs.window.focusedWindow():moveOneScreenNorth()
-end)
-
-hs.hotkey.bind(shiftMash, "l", function()
-	hs.window.focusedWindow():moveOneScreenEast()
-end)
-
 hs.hotkey.bind(mash, "v", function()
 	toggleVPN("Octopart VPN (deprecated)")
 end)
