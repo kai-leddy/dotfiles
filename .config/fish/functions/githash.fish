3f2a964168d7d504910798a4711e1790342d5348
Kai Leddy <epickaimail@gmail.com>
2025-02-03 14:53:23 +0000

    Fix some minor issues

--- .config/fish/functions/githash.fish
+++ .config/fish/functions/githash.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function githash --description 'Use FZF to select a git commit hash'
-	git log --color=always --format="%C(auto)%h %<(15,trunc)%an %s %C(black)%C(bold)%cr %C(auto)%d" | fzf | awk '{print $1}'
+    git log --color=always --format="%C(auto)%h %<(15,trunc)%an %s %C(black)%C(bold)%cr %C(auto)%d" | fzf --ansi | awk '{print $1}'
 end
