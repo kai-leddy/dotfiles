5703ca013a9caf52700ac58f2f0960aedfca6928
Kai Leddy <epickaimail@gmail.com>
2025-02-25 12:15:56 +0000

    Update TODOs in Sketchybar

--- .config/sketchybar/items/init.lua
+++ .config/sketchybar/items/init.lua
@@ -4,7 +4,7 @@ require("items.volume")
 require("items.battery")
 require("items.vpn")
 
--- TODO: add an item for AeroSpace workspaces # # TODO: add some CPU, mem, etc monitors, perhaps with a graph over time
--- # # TODO: add some more icons for status of Bluetooth, VPN, WiFi, etc
--- # # TODO: add some indicators for number of Slack messages, emails, PRs to review, etc
+-- TODO: add some CPU, mem, etc monitors, perhaps with a graph over time
+-- TODO: add some more icons for status of Bluetooth, VPN, WiFi, etc
+-- TODO: add some indicators for number of Slack messages, emails, PRs to review, etc
 -- TODO: add an item showing how long until my next meeting and the title
