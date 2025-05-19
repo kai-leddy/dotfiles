8ff92f22ccf8a00630bc6030652f2612e694d0dc
Kai Leddy <epickaimail@gmail.com>
2025-05-19 11:42:21 +0100

    fix project error on nvim startup

--- .config/nvim/lua/plugins/buffers.lua
+++ .config/nvim/lua/plugins/buffers.lua
@@ -1,11 +1,4 @@
 return {
-  {
-    "ahmedkhalf/project.nvim",
-    opts = {
-      -- auto changing the cwd isn't actually that helpful
-      manual_mode = true,
-    },
-  },
   -- disabled in favour of harpoon2
   { "akinsho/bufferline.nvim", enabled = false },
 
