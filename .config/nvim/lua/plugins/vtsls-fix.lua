726762cd17851ce237f1d82bf0f41c9b117824b2
Kai Leddy <epickaimail@gmail.com>
2025-07-08 15:47:23 +0100

    Various minor updates

--- .config/nvim/lua/plugins/vtsls-fix.lua
+++ .config/nvim/lua/plugins/vtsls-fix.lua
@@ -1,15 +1,45 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  -- REMOVE THIS once this issue is fixed: https://github.com/yioneko/vtsls/issues/159
-  opts = {
-    routes = {
-      {
-        filter = {
-          event = "notify",
-          find = "Request textDocument/inlayHint failed",
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    -- REMOVE THIS once this issue is fixed: https://github.com/yioneko/vtsls/issues/159
+    opts = {
+      routes = {
+        {
+          filter = {
+            event = "notify",
+            find = "Request textDocument/inlayHint failed",
+          },
+          opts = { skip = true },
         },
-        opts = { skip = true },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        vtsls = {
+          settings = {
+            vtsls = {
+              experimental = {
+                completion = {
+                  entriesLimit = 100,
+                },
+              },
+            },
+            typescript = {
+              tsserver = {
+                maxTsServerMemory = 12 * 1024, -- 12GB
+              },
+              preferences = {
+                autoImportFileExcludePatterns = {
+                  "node_modules",
+                },
+              },
+            },
+          },
+        },
       },
     },
   },
