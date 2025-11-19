7bd5f9a07dbe1e6d4f9154b52c5f498feca87955
Kai Leddy <epickaimail@gmail.com>
2025-11-19 10:48:49 +0000

    remove opam config, i dont really do ocaml anymore

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -147,11 +147,3 @@ if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
-
-# BEGIN opam configuration
-# This is useful if you're using opam as it adds:
-#   - the correct directories to the PATH
-#   - auto-completion for the opam binary
-# This section can be safely removed at any time if needed.
-test -r '/Users/kaileddy/.opam/opam-init/init.fish' && source '/Users/kaileddy/.opam/opam-init/init.fish' >/dev/null 2>/dev/null; or true
-# END opam configuration
