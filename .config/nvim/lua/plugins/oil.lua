d64195b93e2b2301f59d4b9d085d17c51a2a56af
Kai Leddy <epickaimail@gmail.com>
2026-02-06 12:25:25 +0000

    Swap Neotree for Oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,21 +1,29 @@
 return {
-  "stevearc/oil.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys = {
-    { "<leader>e", "<cmd>Oil<CR>", desc = "Explorer" },
-    { "-", "<cmd>Oil<CR>", desc = "Explorer" },
+  {
+    -- Disable neo-tree in favour of oil.nvim
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
   },
-  opts = {
-    default_file_explorer = true,
-    view_options = {
-      show_hidden = true,
-      -- This function defines what will never be shown, even when `show_hidden` is set
-      is_always_hidden = function(name, bufnr)
-        return name:match("^%.%.$")
-      end,
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    keys = {
+      { "<leader>e", "<cmd>Oil<CR>", desc = "Explorer" },
+      { "-", "<cmd>Oil<CR>", desc = "Explorer" },
     },
-    float = {
-      padding = 5,
+    opts = {
+      default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+        -- This function defines what will never be shown, even when `show_hidden` is set
+        is_always_hidden = function(name, bufnr)
+          return name:match("^%.%.$")
+        end,
+      },
+      float = {
+        padding = 5,
+      },
     },
   },
 }
