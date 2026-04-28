80a50b8891acd8c145dbabd48d94a0057f281ab0
Kai Leddy <epickaimail@gmail.com>
2026-04-28 14:18:41 +0100

    add env sourcing script

---
+++ .config/fish/functions/envsource.fish
@@ -0,0 +1,7 @@
+function envsource --description 'Source environment variables from a .env file'
+    for line in (cat $argv | grep -v '^#' |  grep -v '^\s*$' | sed -e 's/=/ /' -e "s/'//g" -e 's/"//g' )
+        set export (string split ' ' $line)
+        set -gx $export[1] $export[2]
+        echo "Exported key $export[1]"
+    end
+end
