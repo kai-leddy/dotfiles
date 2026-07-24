1ca01f2f6d7a5e58fc9c5469d8462baa763da321
Kai Leddy <epickaimail@gmail.com>
2026-07-24 16:13:50 +0100

    add kulala vim plugin for rest calls

---
+++ .config/nvim/lua/plugins/kulala.lua
@@ -0,0 +1,11 @@
+return {
+  "mistweaverco/kulala.nvim",
+  opts = {
+    default_env = "dev",
+    request_timeout = 10000, -- 10s timeout by default
+    global_keymaps = true, -- enable default keymaps
+    ui = {
+      display_mode = "float", -- floating or split
+    },
+  },
+}
